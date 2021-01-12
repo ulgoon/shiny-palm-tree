9ba31364eebf9bc4d98f4f9255a35270e0b3aace
kingwangzzang1234 <wychoi0408@naver.com>
2021-01-12 08:36:04 +0000

    feat: Add instruction about fizzbuzz problem

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+
+def do_fizzbuzz():
+    """
+    while the number starts with 1 and up to 100.
+    print 'fizz' if the number is times of three.
+    print 'buzz' if the number is times of five.
+    print 'fizzbuzz' if the number is times of fifteen.
+    else, print the number.
+    """
+    # Fizzbuzz with if and for
+
+    # Fizzbuzz with list comprehension
+
+    # Fizzbuzz in one line
+
+    # Fizzbuzz with lambda function
+
+if __name__=='__main__':
+    do_fizzbuzz()
